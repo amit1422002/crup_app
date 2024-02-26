ebfac0bd7d6ce4749c72ec02b3c2acbd778ab8cc
Amit Singh <Amit@A590-7A04.9431>
2024-02-26 19:03:44 +0530

    Added firebase connection

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,3 @@
- 
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:crudapp/pages/emp.dart';
 import 'package:crudapp/service/database.dart';
@@ -13,67 +11,96 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController namecontroller = new TextEditingController();
+  TextEditingController agecontroller = new TextEditingController();
+  TextEditingController locationcontroller = new TextEditingController();
   Stream? EmployeeStream;
-getontheload()async{
-  EmployeeStream=await DatabaseMethods().getEmpDetails();
-  setState(() {
-    
-  });}
+  getontheload() async {
+    EmployeeStream = await DatabaseMethods().getEmpDetails();
+    setState(() {});
+  }
+
   @override
   void initState() {
-   getontheload();
+    getontheload();
     super.initState();
   }
+
   Widget allEmpDetails() {
     return StreamBuilder(
         stream: EmployeeStream,
         builder: (context, AsyncSnapshot snapshot) {
           return snapshot.hasData
               ? ListView.builder(
-                    itemCount: snapshot.data.docs.length,
-                    itemBuilder: (context, index) {
-                      DocumentSnapshot ds=snapshot.data.docs[index];
-                      return  Container(
-                        margin: EdgeInsets.only(bottom: 10),
-                        child: Material(
-                           
-                                      elevation: 5,
-                                      borderRadius: BorderRadius.circular(10),
-                                      child: Container(
-                                          padding: EdgeInsets.all(20),
-                                          width: MediaQuery.of(context).size.width,
-                                          decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                                          child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.start,
-                                            children: [
-                        Text(
-                          "Name: "+ds["Name"],
-                          style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Age: "+ds["Age"],
-                          style: TextStyle(
-                              color: Colors.orange,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "Location: "+ds["Location"],
-                          style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                                            ],
-                                          )),
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot ds = snapshot.data.docs[index];
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 10),
+                      child: Material(
+                        elevation: 5,
+                        borderRadius: BorderRadius.circular(10),
+                        child: Container(
+                            padding: EdgeInsets.all(20),
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                 
+                                  children: [
+                                    Text(
+                                      "Name: " + ds["Name"],
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
                                     ),
-                      );
-                    })
+                                    Spacer(),
+                                    GestureDetector(
+                                      onTap: () {
+                                        namecontroller.text=ds["Name"];
+                                        agecontroller.text=ds["Age"];
+                                        locationcontroller.text=ds["Location"];
+                                        EditEmployeeDetail(ds["Id"]);
+                                      },
+                                      child: Icon(
+                                        Icons.edit,
+                                        color: Colors.orange,
+                                      ),
+                                     
+                                    ),
+                                    SizedBox(width: 8,),
+                                    GestureDetector(
+                                      onTap: ()async{
+                                        await DatabaseMethods().deleteEmployeeDetail(ds["Id"]);
+
+                                      },
+                                      child: Icon(Icons.delete,color: Colors.orange,))
+                                  ],
+                                ),
+                                Text(
+                                  "Age: " + ds["Age"],
+                                  style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Location: " + ds["Location"],
+                                  style: TextStyle(
+                                      color: Colors.blue,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            )),
+                      ),
+                    );
+                  })
               : Container();
         });
   }
@@ -112,12 +139,116 @@ getontheload()async{
       body: Container(
         margin: EdgeInsets.only(left: 10, right: 10),
         child: Column(
-          children: [
-            Expanded(child: allEmpDetails())
-            
-          ],
+          children: [Expanded(child: allEmpDetails())],
         ),
       ),
     );
   }
+
+  Future EditEmployeeDetail(String id) => showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+            content: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                Row( 
+                  
+                  children: [
+                    
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.cancel)),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Text(
+                      "Edit",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Details",
+                      style: TextStyle(
+                          color: Colors.orange,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                Text(
+                  "Name",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: namecontroller,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Age",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: agecontroller,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Location",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Container(
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: TextField(
+                    controller: locationcontroller,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Center(child: ElevatedButton(onPressed: ()async{
+                   Map<String, dynamic>updateInfo={
+                    "Name":namecontroller.text,
+                    "Age":agecontroller.text,
+                    "Id":id,
+                    "Location":locationcontroller.text,           
+                            };
+                            await DatabaseMethods().updateEmployeeDetail(id, updateInfo).then((value){
+                              Navigator.pop(context);
+                            });
+                }, child: Text("Update")))
+              ]),
+            ),
+          ));
 }
--- lib/service/database.dart
+++ lib/service/database.dart
@@ -2,13 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class DatabaseMethods {
+  // Create
   Future addEmpDetails(Map<String, dynamic> EmpInfoMap, String id) async {
     return await FirebaseFirestore.instance
         .collection("Employee")
         .doc(id)
         .set(EmpInfoMap);
   }
+
+  //Read
   Future<Stream<QuerySnapshot>> getEmpDetails()async{
     return await FirebaseFirestore.instance.collection("Employee").snapshots();
+  }
+
+  //Update
+  Future updateEmployeeDetail(String id, Map<String, dynamic> updateInfo)async{
+    return await FirebaseFirestore.instance.collection("Employee").doc(id).update(updateInfo);
+  }
+  //Delete
+   Future deleteEmployeeDetail(String id)async{
+    return await FirebaseFirestore.instance.collection("Employee").doc(id).delete();
   }
 }
